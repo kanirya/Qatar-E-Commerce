82e344b6ffc5f8c1ae21f163c892683561fbd660
Muhammad <danishkanirya9999@gmail.com>
2025-04-13 00:53:13 +0500

    Add project files.

---
+++ Qatar E-Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qatar_E_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
