1518815d048e411597954e8e095adcb667088fee
Muhammad <danishkanirya9999@gmail.com>
2025-04-14 02:25:47 +0500

    adding Authentication and roles manager

--- Qatar E-Commerce/Program.cs
+++ Qatar E-Commerce/Program.cs
@@ -1,10 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Qatar_E_Commerce.Areas.Identity.Data;
+using Qatar_E_Commerce.Data;
+using Qatar_E_Commerce.Services;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+});
 var app = builder.Build();
-
+await SeedServices.SeedDatabase(app.Services);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -16,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
@@ -24,5 +41,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
+app.MapRazorPages();
 app.Run();
---
+++ Qatar E-Commerce/Services/SeedServices.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Qatar_E_Commerce.Data;
+
+namespace Qatar_E_Commerce.Services
+{
+    public class SeedServices
+    {
+        public static async Task SeedDatabase(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var context=scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var roleManager=scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var logger=scope.ServiceProvider.GetRequiredService<ILogger<SeedServices>>();
+
+            try
+            {
+                logger.LogInformation("Ensuring database is created");
+               await context.Database.EnsureCreatedAsync();
+                logger.LogInformation("Seeding Role");
+                await AddRoleAsync(roleManager, "Admin");
+                await AddRoleAsync(roleManager, "User");
+                await AddRoleAsync(roleManager, "Product Manager");
+                await AddRoleAsync(roleManager, "Customer Support");
+                logger.LogInformation("Seeding admin User");
+                var adminEmail= "admin@gmail.com";
+                if(await userManager.FindByEmailAsync(adminEmail) == null)
+                {
+                    var adminUser = new IdentityUser
+                    {
+                        UserName = adminEmail,
+                        Email = adminEmail,
+                        EmailConfirmed = true,
+                        NormalizedUserName = adminEmail.ToUpper(),
+                        NormalizedEmail = adminEmail.ToUpper(),
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                    };
+                    var result = await userManager.CreateAsync(adminUser, "Admin@123");
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(adminUser, "Admin");
+                        logger.LogInformation("Admin User Created");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+        }
+        private static async Task AddRoleAsync(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(roleName));
+                if(!result.Succeeded)
+                {
+                    throw new Exception($"Failed to create role {roleName}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                }
+            
+            }
+        }
+    }
+}
